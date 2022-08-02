d64826c7230c0aff5cf1c813eff2e1d20043d228
temitayo65 <iloritemitayo65@gmail.com>
2022-08-02 02:11:20 +0100

    Include rate button and remove audio session instance

--- sgbc/Controller/PlayerViewController.swift
+++ sgbc/Controller/PlayerViewController.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-import AVFoundation
-import AVKit
+import SwiftAudio
+import AVFAudio
 
 class PlayerViewController: UIViewController {
 
@@ -16,29 +16,46 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var sermonTitle: UILabel!
     @IBOutlet weak var preacherTitle: UILabel!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var audioProgressView: UIProgressView!
     
     var sermonUpdate: Any = ""
     var sermonGetter = SermonManager() // trying out the sermon manager - still needs updating / refactoring
-    var player: AVPlayer?
-    var playerItem: AVPlayerItem?
-    var PlayButtonIsOff: Bool = false
-    var timeBeingPlayed: Double!
-    
-    // For Playing sounds
-    var audioFile: AVAudioFile!
-    var audioEngine:AVAudioEngine = AVAudioEngine()
-    var audioPlayerNode = AVAudioPlayerNode()
-    // var audioEngine:AVAudioEngine!
-    //var audioPlayerNode: AVAudioPlayerNode!
-    var stopTimer: Timer!
+    var player: AudioPlayer!
+    var playIsOn: Bool = false
+    var playerIsLoaded: Bool = false
+    var audioItem: DefaultAudioItem!
+    var copiedURL: String = ""
+    var currentURL: String! // this will be passed from the tableview when tapped
+
 
     
  
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.27, green: 0.37, blue: 0.46, alpha: 1.0)
-        setLoadView(data: sermonUpdate)
-        setupAudio() // audio is setup once view is loaded 
+        setLoadView(data: sermonUpdate) // what does this do ?
+        
+        
+        player = AudioPlayer()
+        player.timeEventFrequency = .everySecond
+        player.event.updateDuration.addListener(self, handleAudioPlayerTimeEvent)
+        
+        // player.event.stateChange.addListener(self, handleAudioPlayerStateChange)
+        
+        
+        // The audioitem here then becomes the currentURL loaded from the tap on the table cell
+        // This will be compared to the copiedURL inherent in the tableViewController
+        // The comparison will be such that if they are same, then nothing new is loaded
+        // else, the previous audio player session will be stopped and something new will be loaded
+        /*
+         
+         if copiedURL != currentURL{
+         audioItem = DefaultAudioItem(audioUrl: currentURL, sourceType: .stream)
+         }
+         
+         */
+        audioItem = DefaultAudioItem(audioUrl: "https://sgbc.ams3.digitaloceanspaces.com/January-2021/Assurance-of-Having-Eternal-Life-2021-01-03.mp3?AWSAccessKeyId=C663TNSAPB6NR24LMYTF&Expires=1659451449&Signature=c9QYAgQUMKimffUwr19wV%2BvCSvk%3D", sourceType: .stream)
+        print(player.currentTime)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,23 +67,74 @@ class PlayerViewController: UIViewController {
         tabBarController?.tabBar.isHidden = false
     }
     
-    @IBAction func playAudio(_ sender: Any) {
-        // Testing out the feature of the sermon manager
-        // let url = URL(string: "https://sgbc.ams3.digitaloceanspaces.com/Audio/March-2021/Steadfastness-in-Church-Membership.mp3?AWSAccessKeyId=C663TNSAPB6NR24LMYTF&Expires=1658774453&Signature=vLMh6EhCLf4xojH4ojydWplS0YA%3D")
-        /* Handle any work that's necessary after playback. */
-        if !PlayButtonIsOff  || !audioEngine.isRunning{
-            print("Now playing")
-            playButton.setImage(UIImage(systemName: "pause.circle.fill"), for: .normal)
-            PlayButtonIsOff = true
-            playSound(rate: 1.0, pitch: nil, echo: false, reverb: false)
+    func handleAudioPlayerStateChange(state: AudioPlayerState) {
+        
+//        DispatchQueue.main.async {
+//            [self] in
+//            self.player.rate = 2.0
+//            self.player.updateNowPlayingPlaybackValues()
+//        }
+            // Handle the event
+        switch state {
+        case .buffering:
+            print("Audio is still buffering")
+            return
+        case .playing:
+            print("Audio is still playing")
+            return
+        case .paused:
+            print("Audio has been paused")
+            return
+            
+        case .idle:
+            print("Engine is idle")
+            return
+        case .ready:
+            print("Engine is ready to start")
+            return
+        case .loading:
+            print("Engine is loading")
+            return
+        }
+        
+    }
+    
+    func handleAudioPlayerTimeEvent(event: AudioPlayer.UpdateDurationEventData){
+        print("The current time is: ", player.currentTime)
+        print("The duration is: ", player.duration)
+        print("The fraction is: ", player.currentTime/player.duration)
+        DispatchQueue.main.async { [self] in
+            self.audioProgressView.setProgress(Float(self.player.currentTime/self.player.duration), animated: true)
             
         }
-        else if PlayButtonIsOff && audioEngine.isRunning{
-            print("Pausing the Play")
-            playButton.setImage(UIImage(systemName: "play.circle.fill"), for: .normal)
-            PlayButtonIsOff = false
-            pauseAudio()
+        
+        
+    }
+    
+    @IBAction func playAudio(_ sender: Any) {
+        player.event.stateChange.addListener(self, handleAudioPlayerStateChange)
+        player.event.updateDuration.addListener(self, handleAudioPlayerTimeEvent)
+        
+        if !playerIsLoaded{
+            try! player.load(item: audioItem, playWhenReady: true)
+            player.play()
+            playerIsLoaded = true
         }
+        if !playIsOn{
+            player.play()
+            playIsOn = true
+        }
+        else{
+            player.pause()
+            playIsOn = false
+        }
+        
+    
+    }
+    
+    @IBAction func rateButtonPressed(_ sender: UIButton) {
+        // write logic to increase rate here. This should affect whatis in the DispatchQueue.main.async
+        
     }
     
     
